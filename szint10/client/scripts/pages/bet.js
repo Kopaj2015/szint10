8c96df1e8701c0dd834f3ed8e59cc2f92ca50233
Gabor Takacs <atakacsgabor@gmail.com>
2015-11-18 02:29:18 +0100

    bet módosítás

--- szint10/client/scripts/pages/bet.js
+++ szint10/client/scripts/pages/bet.js
@@ -2,14 +2,13 @@ Template.kmkEvents.helpers({
     'myBets': function(user_id){
         Meteor.Fogadas.find({ "Fogadas.user_id" : user_id });
     },
-    'allVerseny': function(){
-        Meteor.Verseny.insert({ esemeny_id: "1", user_id1: "1", user_id2: "2", eredmeny: "1", kategoria: "1" });
-        Verseny.findAll();
+    'allFogadas': function(){
+        return Fogadas.findAll();
     }
 });
-
-function newBet(esemeny_id, value){
-    console.log(esemeny_id + " " + value);
-//Update fogadas, ahol a user_id az act user-e és az esemeny_id a kapott, value-ra allitani a fogast erteket
-//    Fogadas.update();
-}
+Template.adminEsemeny.events({
+    'submit .new-bet': function (event) {
+        event.preventDefault();
+        //TODO bet mentese
+    },
+});
